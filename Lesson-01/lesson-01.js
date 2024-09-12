a6090eb2515580d58d9039e528d94d592eac6b4d
Dominika <85825316+DominikaKolackovska@users.noreply.github.com>
2024-09-12 09:00:07 +0200

    update

---
+++ Lesson-01/lesson-01.js
@@ -0,0 +1,16 @@
+const a = 2;
+const b = 3;
+const c = 4;
+const d = 0;
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+
+console.log(c+d)
+console.log(-d-b)
+console.log(d*b)
+console.log(d/b)
+console.log(-d*b)
+console.log(-d/b)
