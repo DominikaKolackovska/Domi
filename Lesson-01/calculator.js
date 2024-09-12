be44a5a8a8e442fdd14177232a2f5d8cbf5f9b1a
Dominika <85825316+DominikaKolackovska@users.noreply.github.com>
2024-09-12 08:40:42 +0200

    update

--- Lesson-01/calculator.js
+++ Lesson-01/calculator.js
@@ -1,4 +1,22 @@
-const a = (2);
-const b = (4);
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
 
-console.log (a+b)
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
